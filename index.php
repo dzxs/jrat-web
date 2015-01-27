35a01789790fa123b4fb9fa84503e3902d2c8b0a
redpois0n <red@cmail.nu>
2015-01-27 08:18:30 +0100

    Old table removed, formatted

--- index.php
+++ index.php
@@ -6,18 +6,18 @@ require_once "request.php";
 $request = new Request();
 $sock = $request->createSocket();
 
-if (isset($_POST['select']) && isset($_POST['action'])) {
-	$action = $_POST['action'];
+if (isset($_POST ['select']) && isset($_POST ['action'])) {
+	$action = $_POST ['action'];
 	
 	if ($action == "disconnect") {
 		$request->write($sock, 10);
-		$request->write($sock, count($_POST['select']));
+		$request->write($sock, count($_POST ['select']));
 	}
-	foreach ($_POST['select'] as $slave) {
+	foreach ( $_POST ['select'] as $slave ) {
 		$request->write($sock, $slave);
 	}
 } else {
-	
+
 }
 
 $slaves = $request->getSlaves($sock);
@@ -35,6 +35,22 @@ $request->disconnect($sock);
 <link rel="stylesheet" href="layout/jquery.pnotify.default.css">
 <link rel="stylesheet" href="layout/proton.css">
 <link rel="stylesheet" href="layout/select2.css">
+
+<script>
+$(document).ready(function() {
+    $('#selectall').click(function(event) {  //on click 
+        if(this.checked) { // check select status
+            $('.box').each(function() { //loop through each checkbox
+                this.checked = true;  //select all checkboxes with class "checkbox1"               
+            });
+        }else{
+            $('.box').each(function() { //loop through each checkbox
+                this.checked = false; //deselect all checkboxes with class "checkbox1"                       
+            });         
+        }
+    });
+    
+});</script>
 
 
 <div class="panel panel-default panel-block">
@@ -57,7 +73,7 @@ $request->disconnect($sock);
 					<tbody>
 	
 					<?php
-						/*
+					/*
 					 * $i = 1;
 					 * foreach ($slaves as $slave) {
 					 * echo "<tr>" . str_replace("%c", $i++ & 1 ? "tg-vn4c" : "tg-031e", $slave->getTableFormatted($i)) . "</tr>\n";
@@ -73,117 +89,26 @@ $request->disconnect($sock);
 						echo printOut($slave->getIdentifier());
 						echo printOut($slave->getIP());
 						echo printOut($slave->getOperatingSystem());
-						echo printOut("<a href='bots.php?id=" . $slave->getUniqueId () . "'><button type='button'class='btn btn-xs btn-danger'>Delete</button></a>");
+						echo printOut("<a href='bots.php?id=" . $slave->getUniqueId() . "'><button type='button'class='btn btn-xs btn-danger'>Delete</button></a>");
 						
 						echo "</tr>\n";
-						
+					
 					}
 					?>
 	
 					</tbody>
 				</table>
-<div class="form-group" align="right">
-                                    <div>
-                                    	<ul class="pagination pagination-demo">
-										  <li><a href="bots.php?page=0">&laquo;</a></li>
-<li><a href='bots.php?page=1'>1</a></li>                                    	  <li><a href="bots.php?page=2">&raquo;</a></li>
-                                    	</ul>
-                                    </div>
-                                </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+				<div class="form-group" align="right">
+					<div>
+						<ul class="pagination pagination-demo">
+							<li><a href="bots.php?page=0">&laquo;</a></li>
+							<li><a href='bots.php?page=1'>1</a></li>
+							<li><a href="bots.php?page=2">&raquo;</a></li>
+						</ul>
+					</div>
 				</div>
-				
-
-				<script>
-$(document).ready(function() {
-    $('#selectall').click(function(event) {  //on click 
-        if(this.checked) { // check select status
-            $('.box').each(function() { //loop through each checkbox
-                this.checked = true;  //select all checkboxes with class "checkbox1"               
-            });
-        }else{
-            $('.box').each(function() { //loop through each checkbox
-                this.checked = false; //deselect all checkboxes with class "checkbox1"                       
-            });         
-        }
-    });
-    
-});</script>
-
-				<!-- <style type="text/css">
-	.tg {
-		border-collapse: collapse;
-		border-spacing: 0;
-		border-color: #aabcfe;
-	}
-	
-	.tg td {
-		font-family: Arial, sans-serif;
-		font-size: 14px;
-		padding: 10px 5px;
-		border-style: solid;
-		border-width: 1px;
-		overflow: hidden;
-		word-break: normal;
-		border-color: #aabcfe;
-		color: #669;
-		background-color: #e8edff;
-	}
-	
-	.tg th {
-		font-family: Arial, sans-serif;
-		font-size: 14px;
-		font-weight: normal;
-		padding: 10px 5px;
-		border-style: solid;
-		border-width: 1px;
-		overflow: hidden;
-		word-break: normal;
-		border-color: #aabcfe;
-		color: #039;
-		background-color: #b9c9fe;
-	}
-	
-	.tg .tg-0ord {
-		text-align: right
-	}
-	
-	.tg .tg-ifyx {
-		background-color: #D2E4FC;
-		text-align: right
-	}
-	
-	.tg .tg-s6z2 {
-		text-align: center
-	}
-	
-	.tg .tg-vn4c {
-		background-color: #D2E4FC
-	}
-</style> -->
-
-				<form method="post">
-					<table class="tg">
-						<tr>
-							<th class="tg-s6z2">Country</th>
-							<th class="tg-s6z2">Display Name</th>
-							<th class="tg-s6z2">Operating System</th>
-							<th class="tg-s6z2">Host</th>
-							<th class="tg-s6z2"><input type="checkbox" class="box"
-								id="selectall"></th>
-						</tr>
-	
-		<?php 
-			$i = 1;
-			foreach ($slaves as $slave) {
-				echo "<tr>" . str_replace("%c", $i++ & 1 ? "tg-vn4c" : "tg-031e", $slave->getTableFormatted($i)) . "</tr>\n";
-			}
-		?>
-	</table>
-
-					<input type="submit" name="action" value="disconnect">Disconnect
-					</button>
-				</form>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
